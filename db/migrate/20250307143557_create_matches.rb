008b35d5d2712a4c4c55e094267df64c2d4d47bb
Sterling Tran <devtranminhtu@gmail.com>
2025-03-07 21:47:29 +0700

    add stadium_id into match to manage both

--- db/migrate/20250307143557_create_matches.rb
+++ db/migrate/20250307143557_create_matches.rb
@@ -5,6 +5,7 @@ class CreateMatches < ActiveRecord::Migration[8.0]
       t.string :team2
       t.integer :score1
       t.integer :score2
+      t.integer :stadium_id
 
       t.timestamps
     end
