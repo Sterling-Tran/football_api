a456e3f53788227f67acb15d4c26201920722414
Sterling Tran <devtranminhtu@gmail.com>
2025-03-07 22:09:26 +0700

    add some action into match controller to control match

---
+++ app/controllers/matches_controller.rb
@@ -0,0 +1,13 @@
+class MatchesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get "matches/index"
+  get "matches/show"
+  get "matches/create"
+  get "matches/update"
   get "/player" => "player#index", as: :player
 
   # root "posts#index"
