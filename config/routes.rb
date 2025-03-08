1a757b9e9a90c326deb83af7067a6e30d5db938b
Sterling Tran <devtranminhtu@gmail.com>
2025-03-08 09:13:28 +0700

    create route controller model Team

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get "teams/index" => "teams#index"
+  get "teams/create" => "teams#create"
+  get "teams/show" => "teams#show"
+  get "teams/destroy" => "teams#destroy"
+  get "teams/update" => "teams#update"
   get "matches/index" => "matches#index"
   get "matches/show" => "matches#show"
   get "matches/create" => "matches#create"
