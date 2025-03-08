bee8ed57289284ff9a14050626af13eb5415a1bb
Sterling Tran <devtranminhtu@gmail.com>
2025-03-08 08:47:01 +0700

    create matches controller and routing

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,13 +1,34 @@
 class MatchesController < ApplicationController
   def index
+    @matches = Match.all
   end
 
   def show
+    @match = Match.find(params[:id])
   end
 
   def create
+    @data = {
+      team1: params[:team1],
+      team2: params[:team2],
+      score1: params[:score1],
+      score2: params[:score2],
+      stadium_id: params[:stadium_id]
+    }
+    @match = Match.new(@data)
+    @match.save
   end
 
   def update
+    @match = Match.find(params[:id])
+    @data = 
   end
+
+
+  def destroy
+    @match = Match.find(params[:id])
+    @match.destroy
+  end
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get "matches/index"
-  get "matches/show"
-  get "matches/create"
-  get "matches/update"
+  get "matches/index" => "matches#index"
+  get "matches/show" => "matches#show"
+  get "matches/create" => "matches#create"
+  get "matches/update" => "matches#update"
   get "/player" => "player#index", as: :player
 
   # root "posts#index"
